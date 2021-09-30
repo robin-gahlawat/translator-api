04c13be29acabb3e8ed8499585536844dcc39750
robin <robingahlawat30@gmail.com>
2021-09-30 20:03:09 +0530

    more changes

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,15 +1,7 @@
 
 const mongoose = require('mongoose');
 
-// mongoose.connect('mongodb://127.0.0.1:27017/translation-api',{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
-
-
-// mongodb+srv://testdb:testdb123@cluster0.uxyzv.mongodb.net/shopperDb?retryWrites=true&w=majority
-
-mongoose.connect('mongodb+srv://testdb:testdb123@cluster0.uxyzv.mongodb.net/translationAPI?retryWrites=true&w=majority',{
+mongoose.connect('mongodb://127.0.0.1:27017/translation-api',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
